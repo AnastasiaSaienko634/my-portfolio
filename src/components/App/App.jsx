3aa2b557a88eb5b29785becc6921c28e44b430ba
nasti <anastasiasaienko634@gmail.com>
2025-07-01 21:59:15 +0200

    update all

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../Header/Header";
+import { Hero } from "../Hero/Hero";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+}
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,9 @@
+export function Hero() {
+  return (
+    <>
+      <h1 className="flex text-white mx-auto justify-center p-40">
+        My Portfolio soon...
+      </h1>
+    </>
+  );
+}
